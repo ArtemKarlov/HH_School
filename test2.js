7406ef931b429aebd422c923b22e8b20c4ca39ab
ArtemKarlov <ar.karlov@gmail.com>
2020-10-17 10:37:23 +0400

    ver2

--- test2.js
+++ test2.js
@@ -1,12 +1,11 @@
 "use strict";
 
+// const line = `1
+// 1595862781 1595862785`;
 
-const line = `1
-1595862781 1595862785`;
-
-// const line = `2
-// 1595862781 1595862783
-// 1595862782 1595862784`;
+const line = `2
+1595862781 1595862783
+1595862782 1595862784`;
 
 // const line = `2
 // 1595862781 1595862782
@@ -18,37 +17,13 @@ const line = `1
 // 103 104
 // 105 108`;
 
-try {
-    await generateString();
-}
-catch {
-    console.log('Error');
-}
+const result = getResult(line);
+console.log(typeof(result));
 
+console.log(String(result));
 
-console.log(main(line));
-
-
-async function generateString() {
-    const target = ['2', '1595862781 1595862783', '1595862782 1595862784'];
-    let line = '';
-    target.forEach((subLine) => {
-        setTimeout(() =>{ 
-            line = line + '\n' + subLine;
-        } ,2000);
-    });
-    console.log(line);
-}
-
-async function main(line) {
-    const result = await getResult(line);
-    console.log(result);
-    return result;
-}
-
-
-async function getResult(line) {
-    const parsedLine = await parseLine(line);
+function getResult(line) {
+    const parsedLine = parseLine(line);
     let intervals = parsedLine.slice(1);
 
     intervals = sortIntervals(intervals);
@@ -69,7 +44,7 @@ async function getResult(line) {
     return findedIntervalsCount + ' ' + findedIntervalsDuration;
 }
 
-async function parseLine(line) {
+function parseLine(line) {
     const splitLine = line.split('\n');
     const intervalsCount = Number(splitLine[0]);
     const intervals = splitLine.slice(1).map(interval => {
@@ -78,8 +53,6 @@ async function parseLine(line) {
 
     return [intervalsCount, ...intervals];
 }
-
-
 
 function reduceIntervals(array) {
     const arr = [...array]
